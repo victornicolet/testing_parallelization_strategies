28de1e26fa2c77f0f2fc5e2c997d9cca028f1034
Victor Nicolet <victorn@cs.toronto.edu>
2018-04-13 17:03:56 -0400

    Update heatmaps.

--- plot_max_top_right_rectangle.py
+++ plot_max_top_right_rectangle.py
@@ -7,8 +7,17 @@ import pandas as pd
 
 data = pd.read_csv(sys.argv[1])
 
-data_by_thread = {}
-max_by_thread = {}
+def plot_df(df, ncores, range_one):
+    heatmap = plt.pcolor(df, cmap='RdBu', vmin=1-range_one, vmax=1 + range_one)
+    plt.title("Running time comparison for split and fused loops (%d cores)" % ncores)
+    plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
+    plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
+    leg = plt.colorbar(heatmap)
+    leg.ax.set_ylabel("Elapsed time split / fused, height vertical.")
+    plt.savefig("figures/mtrr_heatmap_%d_cores" % ncores)
+    plt.clf()
+
+
 
 for i in [1,2,3,4,5,6,7,8,12,16,20,24,32]:
     df = data.loc[data['threads'] == i].drop(['threads'], axis=1)
@@ -17,20 +26,10 @@ for i in [1,2,3,4,5,6,7,8,12,16,20,24,32]:
     tmax = df['split/fused'].max()
     tmin = df['split/fused'].min()
     df = df.pivot(index='height', columns='width',values='split/fused')
-    data_by_thread[i] = df
-    max_by_thread[i] = (tmax, tmin)
+    range_one = max(abs(1-tmax), abs(1-tmin))
+    plot_df(df, i, range_one)
 
 
-def plot_df(df, rng, range_one):
-    heatmap = plt.pcolor(df, cmap='RdBu', vmin=1-range_one, vmax=1 + range_one)
-    plt.title("Running time comparison for split and fused loops (%d)" % ncores)
-    plt.yticks(np.arange(0.5, len(df.index), 1), df.index)
-    plt.xticks(np.arange(0.5, len(df.columns), 1), df.columns)
-    leg = plt.colorbar(heatmap)
-    leg.ax.set_ylabel("Elapsed time split / fused, height vertical.")
-    plt.show()
-    plt.savefig("figures/mtrr_heatmap_%d_cores" % ncores)
 
-ncores = int(sys.argv[2])
-range_one = max(abs(1-max_by_thread[ncores][0]), abs(1-max_by_thread[ncores][1]))
-plt = plot_df(data_by_thread[ncores], ncores, range_one)
+
+
